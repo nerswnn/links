b7151ee383bd664ff5424e3e3208929b8553eb87
nerswnn <cloudstorage.first0@gmail.com>
2025-12-01 10:29:26 +0530

    added index.html

---
+++ index.php
@@ -0,0 +1,41 @@
+<html lang="en"><head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>ND</title>
+  <link rel="icon" type="image" href="ndlogo.png">
+  
+<link rel="stylesheet" href="style.css"></head>
+<body>
+  <div class="container">
+    <div class="profile">
+    
+     <center> <h1>Nerswn Dewry</h1> </center>
+    </div>
+    <div class="links">
+      <a href="https://www.instagram.com/nerswn_dewry_official/" target="_blank">Instagram</a>
+      <a href="https://www.youtube.com/@nerswndewryofficial?sub_confirmation=1" target="_blank">YouTube</a>
+      <a href="https://www.facebook.com/nerswn.dewry.official" target="_blank">Facebook</a>
+      <a href="https://x.com/nerswndewry" target="_blank">Twitter</a>
+       <a href="https://t.me/nerswn_dewry_official" target="_blank">Telegram</a>
+        <a href="https://discord.com/invite/qqh7EDqfUM" target="_blank">Discord</a>
+        </div>
+        
+
+
+      <br>
+ <div class="footer">    
+         <footer>
+    <h5><p> Copyright © 2024-2025<a href="https://donation-me.github.io/donation-me/"> Nerswn Dewry.</a> All Rights Reserved.</p></h5>
+         </footer>
+       
+     </div>
+
+ 
+
+
+
+ 
+
+
+
+</div><script src="script.js"></script></body></html>
---
+++ script.js
@@ -0,0 +1,19 @@
+
+  
+    document.addEventListener('contextmenu', function(e) {
+        e.preventDefault();
+    });
+
+    document.addEventListener('keydown', function(e) {
+        if (e.key === 'F12' || 
+            (e.ctrlKey && e.shiftKey && e.key === 'I') || 
+            (e.ctrlKey && e.key === 'u')
+        ) {
+            e.preventDefault();
+        }
+    });
+
+
+
+
+ 
